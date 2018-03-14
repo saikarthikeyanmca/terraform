490e8b0c3b003ba77dc6174298fa0ebb52c0f382
saikarthikeyanmca <36722876+saikarthikeyanmca@users.noreply.github.com>
2018-03-14 08:30:00 +0530

    Update ec2instances.tf
    update

--- ec2instances.tf
+++ ec2instances.tf
@@ -1,6 +1,6 @@
 provider "aws" {
         access_key = "AKIAJP705EANUVHS2MQA"
-        secret_key = "cZbLG0k8frEk8xDZYM5BX6UU7JrqKHepsTFTjcl6"
+        secret_key = "42:fb:a1:36:51:f1:86:f6:ee:c8:29:be:cb:98:9e:95:76:66:b3:9f"
         region = "ap-southeast-1"
 }
 
